6108ea5a19d17915016ca349da8bff5e640974cf
standardforever <Engr.kusimos>
2026-04-22 10:01:04 +0100

    change port

--- source/main.py
+++ source/main.py
@@ -6,4 +6,4 @@ from api.app import app
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8110, reload=True)
