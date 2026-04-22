37ff5ee316789e58fb6e3eebbda4a63c818b40dd
standardforever <Engr.kusimos>
2026-04-22 10:25:11 +0100

    added roth_path to fastapi

--- source/api/app.py
+++ source/api/app.py
@@ -9,9 +9,10 @@ from utils.logging import configure_logging
 
 def create_app() -> FastAPI:
     configure_logging()
-    app = FastAPI(title="Job Scraper API", version="0.1.0")
+    app = FastAPI(title="Job Scraper API", version="0.1.0", root_path="/ats")
     app.include_router(ats_domain_router, prefix="/api")
     app.include_router(api_router, prefix="/api")
+    
     return app
 
 
